8d42b1466cc627c741207d59e645e06184cfa6aa
MarufSwe <khanmaruf.swe@gmail.com>
2020-07-07 18:19:02 +0600

    pythonanywhere done

--- Api_practices/urls.py
+++ Api_practices/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bike_house.urls')),
-    path('', include('products_category.urls'))
+    path('', include('products_category.urls')),
+    path('', TemplateView.as_view(template_name="home.html")),
 ]
